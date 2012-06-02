ac17d6026a5dabaffe0ca55d0a2ede2514d3773a
rod <rod@pu-gh.com>
2012-06-02 09:05:27 +0100

    added more functions

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 
-(defproject jayq "0.0.1"
-  :description "A ClojureScript wrapper for jQuery UI"
+(defproject jayqui "0.0.2"
+  :description "A ClojureScript wrapper for jQuery UI (1.8.18)"
   :dependencies [[jayq "0.1.0-alpha4"]])
 
--- src/jayqui/core.cljs
+++ src/jayqui/core.cljs
@@ -5,3 +5,12 @@
 (defn draggable [elem props]
   (.draggable elem (clj->js props)))
 
+(defn droppable [elem props]
+  (.draggable elem (clj->js props)))
+
+(defn sortable [elem props]
+  (.sortable elem (clj->js props)))
+
+(defn resizable [elem props]
+  (.resizable elem (clj->js props)))
+
